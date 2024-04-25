7ef7e714d2ac18169567aa423bd69705fac3ee24
KoenCuijp <kghcuijpers@gmail.com>
2024-04-25 09:43:56 +0200

    Add quote activation & logging

--- hubspot-api.py
+++ hubspot-api.py
@@ -101,7 +101,9 @@ class HubspotAPI:
             url=f'{self.get_url(self.DEALS_ENDPOINT)}/{deal_id}',
             params="associations=contacts,line_items"
         )
-        return response.json()
+        existing_deal = response.json()
+        print(f'Got existing deal {deal_id} [status {response.status_code}]: {existing_deal}')
+        return existing_deal
 
     def get_line_item(self, line_item_id: str) -> dict:
         """
@@ -111,7 +113,9 @@ class HubspotAPI:
             method='GET',
             url=f'{self.get_url(self.LINE_ITEMS_ENDPOINT)}/{line_item_id}?properties=name',
         )
-        return response.json()
+        line_item = response.json()
+        print(f'Got line item {line_item_id} [status {response.status_code}]: {line_item}')
+        return line_item
     
     def update_line_item(self, line_item_id: str, number_of_persons: int) -> None:
         """
@@ -128,25 +132,29 @@ class HubspotAPI:
         )
         print(f'Updated line item {line_item_id} with amount {number_of_persons}: Status {response.status_code}')
 
-    def create_quote(self, quote_title: str) -> int:
+    def create_quote(self, quote_title: str, quote_slug: str) -> int:
         """
         Create a quote in Hubspot and return its id
         """
         response = self.do_hubspot_request(
             method='POST',
             url=self.get_url(self.QUOTE_ENDPOINT),
-            payload=self.create_quote_payload(quote_title)
+            payload=self.create_quote_payload(quote_title, quote_slug)
         )
-        return response.json()['id']
+        result = response.json()
+        print(f'Created quote [status {response.status_code}]: {result}')
+        return result['id']
 
-    def create_quote_payload(self, quote_title: str) -> dict[str, dict[str, str]]:
+    def create_quote_payload(self, quote_title: str, quote_slug) -> dict[str, dict[str, str]]:
         """
-        Creates a Hubspot quote playload for the given quote title
+        Creates a Hubspot quote playload for the given quote title & sluge
+            note: slug is used as the last part of the URL for the quote
         """
         expiration_date = datetime.now() + timedelta(days=30)
         return {
             "properties": {
                 "hs_title": quote_title,
+                "hs_slug": quote_slug,
                 "hs_expiration_date": expiration_date.strftime('%Y-%m-%d'), # Example: 2024-12-10
                 "hs_esign_enabled": "true",
                 "hs_status": "DRAFT",
@@ -159,6 +167,22 @@ class HubspotAPI:
             }
         }
     
+    def activate_quote(self, quote_id: int) -> None:
+        """
+        Create a quote in Hubspot and return its id
+        """
+        response = self.do_hubspot_request(
+            method='PATCH',
+            url=f'{self.get_url(self.QUOTE_ENDPOINT)}/{quote_id}',
+            payload={
+                "properties": {
+                    "hs_status": "APPROVAL_NOT_NEEDED",
+                    "hs_slug": f'{quote_id}{datetime.now().strftime("%Y%m%d%H%M%S")}',
+                }
+            }
+        )
+        print(f'Activated quote {quote_id} [Status {response.status_code}]: {response.json()}')
+
     def associate_quote_to(self, quote_id: int, object_type: str, object_id: str) -> None:
         """
         Associate a quote to the given other Hubspot object (e.g. deal, contact, etc)
@@ -172,7 +196,7 @@ class HubspotAPI:
         Associate a quote to the contact that has to sign the quote
         """
         url = f'{self.BASE_URL}/crm/v4/objects/quote/{quote_id}/associations/contact/{contact_id}'
-        self.do_hubspot_request(
+        response = self.do_hubspot_request(
             method='PUT',
             url=url,
             payload=[
@@ -183,7 +207,7 @@ class HubspotAPI:
                 }
             ]
         )
-
+        print(f'Associated quote to signer {contact_id}: {response.status_code}')
 
 """
 START OF LOGIC
@@ -198,6 +222,7 @@ FLOW:
     6. Associate quote w/ lineitem
     7. Associate quote w/ contact
     8. Associate quote w/ quote signer (https://developers.hubspot.com/beta-docs/guides/api/crm/commerce/quotes#associating-quote-signers)
+    9. Activate the quote
 """
 
 HUBSPOT_API = HubspotAPI(token='pat-eu1-b3414dd4-2d72-4832-a44b-b728368e2a2b')
@@ -216,7 +241,10 @@ for line_item_id in line_item_ids:
     HUBSPOT_API.update_line_item(line_item_id, number_of_persons=input_data["NUMBER_OF_PERSONS"])
 
     # 3. CREATE QUOTE
-    quote_id = HUBSPOT_API.create_quote(quote_title=f'{challenge_name} - {input_data["DEAL_NAME"]}')
+    quote_id = HUBSPOT_API.create_quote(
+        quote_title=f'{challenge_name} - {input_data["DEAL_NAME"]}',
+        quote_slug=f'{line_item_id}{datetime.now().strftime("%Y%m%d%H%M%S")}'
+    )
 
     # 4. ASSOCIATE w/ TEMPLATE
     HUBSPOT_API.associate_quote_to(
@@ -248,3 +276,6 @@ for line_item_id in line_item_ids:
 
     # 8. ASSOCIATE w/ SIGNER
     HUBSPOT_API.associate_quote_to_signer(quote_id, contact_id)
+
+    # 9. ACTIVATE QUOTE
+    HUBSPOT_API.activate_quote(quote_id)
